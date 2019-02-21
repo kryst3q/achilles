fe49554dcb68da0a9599dcb90831708ea78a313f
Allan690 <allan690@allan690.usermd.net>
2019-02-21 22:55:12 +0100

    Install & base configure babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-const APP_DIR = path.resolve(__dirname, 'src/client/app');
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const APP_DIR = path.resolve(__dirname, 'src');
 
 const config = {
     entry: APP_DIR + '/index.jsx',
     output: {
         path: BUILD_DIR,
-        filename: 'bundle.js'
+        filename: 'achilles.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: "/\.jsx?/",
+                include: APP_DIR,
+                loader: 'babel'
+            }
+        ]
     }
 };
 
