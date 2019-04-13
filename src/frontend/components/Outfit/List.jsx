d4118b6e8780605603d674105a973f32a592dd84
Allan690 <allan690@allan690.usermd.net>
2019-04-14 01:14:40 +0200

    Start creating Comparision component

--- src/frontend/components/Outfit/List.jsx
+++ src/frontend/components/Outfit/List.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import {getI18n, withTranslation} from 'react-i18next';
+import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
 import ListElement from './ListElement.jsx';
 
 class List extends Component {
@@ -9,6 +10,9 @@ class List extends Component {
         this.state = {
             list: []
         };
+
+        this.handleAddToCompare = this.handleAddToCompare.bind(this);
+        this.handleRemoveFromCompare = this.handleRemoveFromCompare.bind(this);
     }
 
     componentDidMount() {
@@ -36,25 +40,75 @@ class List extends Component {
             });
     }
 
+    handleAddToCompare(event, data) {
+        let toCompare = localStorage.getItem('toCompare');
+
+        if (null === toCompare) {
+            toCompare = [];
+        } else {
+            toCompare = JSON.parse(toCompare);
+
+            if (!toCompare.some((element, index, array) => element.id === data.id && element.model === 'Outfit')) {
+                toCompare.push({id: data.id, model: 'Outfit'});
+            }
+        }
+
+        localStorage.setItem('toCompare', JSON.stringify(toCompare));
+    }
+
+    handleRemoveFromCompare(event, data) {
+        let toCompare = localStorage.getItem('toCompare');
+
+        if (null === toCompare) {
+            return;
+        } else {
+            toCompare = JSON.parse(toCompare);
+        }
+
+        if (toCompare.some((element, index, array) => element.id === data.id && element.model === 'Outfit')) {
+            localStorage.setItem('toCompare', JSON.stringify(toCompare.filter(element => element.id !== data.id && element.model === 'Outfit')));
+        }
+    }
+
     render() {
         const { t } = this.props;
         let list  = this.state.list;
 
         return (
             <div>
-                {
-                    0 < list.length
-                        ?
-                        list.map(element => (
-                            <ListElement
-                                key={element.id.toString()}
-                                src={element.Images[0].File.hash}
-                                names={element.Names}
-                            />
-                        ))
-                        :
-                        <span>{ t('outfitList:noRecords') }</span>
-                }
+                <div>
+                    {
+                        0 < list.length
+                            ?
+                            list.map(element => (
+                                    <ContextMenuTrigger
+                                        id={'context-menu'}
+                                        collect={() => element}
+                                    >
+                                        <ListElement
+                                            key={element.id.toString()}
+                                            src={element.Images[0].File.hash}
+                                            names={element.Names}
+                                        />
+                                    </ContextMenuTrigger>
+                            ))
+                            :
+                            <span>{ t('outfitList:noRecords') }</span>
+                    }
+                </div>
+                <div>
+                    <ContextMenu
+                        id={'context-menu'}
+                        hideOnLeave={true}
+                    >
+                        <MenuItem onClick={this.handleAddToCompare}>
+                            Dodaj do porównania
+                        </MenuItem>
+                        <MenuItem onClick={this.handleRemoveFromCompare}>
+                            Usuń z porównywania
+                        </MenuItem>
+                    </ContextMenu>
+                </div>
             </div>
         );
     }
