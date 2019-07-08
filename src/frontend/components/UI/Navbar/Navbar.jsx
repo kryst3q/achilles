918b16e963d28a9747b40079d4f68b5bacd194fd
Allan690 <allan690@allan690.usermd.net>
2019-07-09 00:38:00 +0200

    Apply different color to active navicon; clean up navicon's svg code

--- src/frontend/components/UI/Navbar/Navbar.jsx
+++ src/frontend/components/UI/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navlink from './Navlink';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const StyledNavbar = styled('div')`
 	float: left;
--- src/frontend/components/UI/Navbar/Navlink.jsx
+++ src/frontend/components/UI/Navbar/Navlink.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import { IndexLinkContainer } from "react-router-bootstrap";
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
 const StyledNavlink = styled('div')`
+	mask: url(${(props) => props.mask});
+	background: black;
 	border: 1px solid black;
 	border-radius: 5px;
-	width: 60px;
-	height: 60px;
+	width: ${(props) => props.width ? props.width : 60}px;
+	height: ${(props) => props.height ? props.height : 60}px;
 	cursor: pointer;
 	margin-bottom: 5px;
 	padding: 4px;
+	&.active {
+		background: red;
+	};
 `;
 
 const Navlink = (props) => {
 	const { to, image, title } = props;
 	const { t } = useTranslation('navigation');
-	const navbarIconHeight = 50;
+	const navbarIconHeight = 60;
 
 	return (
-		<StyledNavlink>
-			<IndexLinkContainer to={to} activeClassName="active">
-				<img
-					src={require(`../../../assets/navbar/${image}.svg`)}
-					title={ t(title) }
-					height={navbarIconHeight}
-				/>
-			</IndexLinkContainer>
-		</StyledNavlink>
+		<IndexLinkContainer to={to} activeClassName="active">
+			<StyledNavlink
+				height={navbarIconHeight}
+				width={navbarIconHeight}
+				mask={require(`../../../assets/navbar/${image}.svg`)}
+			/>
+		</IndexLinkContainer>
 	);
 };
 
